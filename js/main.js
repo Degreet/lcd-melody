87caa4d42e93dc027704ecc75d6e9cb622819df3
Degreet <degreetpro@gmail.com>
2021-08-20 08:47:59 +0300

    floor modal with animation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ const counterSpan = document.querySelector('.main__counter-text')
 const counterUpBtn = document.querySelector('.main__counter-up')
 const counterDownBtn = document.querySelector('.main__counter-down')
 const homePaths = document.querySelectorAll('.main__home path')
+const flatPaths = document.querySelectorAll('.modal__flats path')
+const flatLinks = document.querySelectorAll('.modal__flat-link')
+const modal = document.querySelector('.modal')
+const closeModalBtn = document.querySelector('.modal__close-btn')
+const modalSelectedFloor = document.querySelector('.modal__selected-floor')
+const viewFlatsBtn = document.querySelector('.main__view-flats-btn')
 let currentFloor = 2
+let currentFlat = 40
 
 function saveFloorCounter() {
   homePaths.forEach((el) => el.classList.remove('selected'))
@@ -12,12 +19,43 @@ function saveFloorCounter() {
   })
 }
 
+function saveFlat() {
+  flatPaths.forEach((el) => el.classList.remove('selected'))
+  flatLinks.forEach((el) => el.classList.remove('selected'))
+  document.querySelector(`path[data-flat="${currentFlat}"]`).classList.add('selected')
+  document.querySelector(`.modal__flat-link[data-flat="${currentFlat}"]`).classList.add('selected')
+}
+
+function flatHoverHandler(e) {
+  const selectedFlat = +e.target.dataset.flat
+  currentFlat = selectedFlat
+  saveFlat()
+}
+
+function toggleModal() {
+  modalSelectedFloor.innerText = currentFloor.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+  })
+
+  modal.classList.toggle('open')
+}
+
+flatLinks.forEach((el) => {
+  el.addEventListener('mouseover', flatHoverHandler)
+})
+
+flatPaths.forEach((el) => {
+  el.addEventListener('mouseover', flatHoverHandler)
+})
+
 homePaths.forEach((el) => {
   el.addEventListener('mouseover', (e) => {
     const selectedFloor = +e.target.dataset.floor
     currentFloor = selectedFloor
     saveFloorCounter()
   })
+
+  el.addEventListener('click', toggleModal)
 })
 
 counterUpBtn.addEventListener('click', () => {
@@ -31,3 +69,6 @@ counterDownBtn.addEventListener('click', () => {
   currentFloor--
   saveFloorCounter()
 })
+
+viewFlatsBtn.addEventListener('click', toggleModal)
+closeModalBtn.addEventListener('click', toggleModal)
